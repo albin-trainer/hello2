dcbf4abfaf0592b902ab2de556dbc89f42b66d3f
unknown <albinxavier@gmail.com>
2024-03-01 15:01:43 +0530

    d

---
+++ Sample/src/com/cg/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.cg;
+
+public class HelloWorld {
+public static void main(String[] args) {
+	System.out.println("Hi!!!!!s");
+	int x=10;
+	System.out.println("s s s ");
+	System.out.println("In feature 1");
+}
+}
